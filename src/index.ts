d660a5aa447fbe76a13b6c6c97b89a3586ec197e
Maike Mota <maike_henrique@hotmail.com>
2023-01-09 13:16:54 -0300

    fix: handle error while running runners

--- src/index.ts
+++ src/index.ts
@@ -67,11 +67,13 @@ client.on('authenticated', async () => {
 
     runners.forEach(runnerConstructor => {
         const runner = new runnerConstructor();
-        console.info(`Registering runner ${runner.name} to run every ${runner.runEveryNMinutes} minut(s)`)
-        runner.run(client);
-        const interval = setInterval(() => {
-            runner.run(client)
-        }, runner.runEveryNMinutes * 60 * 1000)
+        console.info(`Registering runner ${runner.name} to run every ${runner.runEveryNMinutes} minute(s)`)
+        const catchFun = (exception) => console.error(`Error while trying to run ${runner.name}`, exception)
+        runner.run(client).catch(catchFun);
+        const interval = setInterval(async () => {
+            await runner.run(client).catch(catchFun);
+        }, runner.runEveryNMinutes * 60 * 1000);
+
         runningRunners.push({
             intervalId: interval,
             runner
