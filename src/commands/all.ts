1aa58f09a061bf4fdfb1be5510b4c1d05746ceb5
Maike Mota <maike_henrique@hotmail.com>
2023-01-19 09:03:15 -0300

    fix: restriction by user

--- src/commands/all.ts
+++ src/commands/all.ts
@@ -5,7 +5,7 @@ const ALL_COMMAND_INTERVAL_IN_MINUTES = parseInt((process.env.ALL_COMMAND_INTERV
 
 const INTERVAL_BETWEEN_USES = ALL_COMMAND_INTERVAL_IN_MINUTES * 60 * 1000
 
-const BANNED_USERS = process.env.ALL_COMMAND_BANNED_USERS.split(',').map(u=> u.trim());
+const BANNED_USERS = process.env.ALL_COMMAND_BANNED_USERS.split(',').map(u => u.trim());
 
 const lastUses = {}
 const lastWarnings = {}
@@ -18,10 +18,14 @@ export class MentionAllCommand implements Command {
  _Marca todos os integrantes do grupo_
 `
     async isValid(chat: Chat, msg: Message, ...argsArray: string[]): Promise<boolean> {
-        const contact = await msg.getContact();
-        return chat.isGroup && BANNED_USERS.includes(contact.number);
+        return true
     }
     async handle(client: Client, chat: GroupChat, msg: Message, ...argsArray: string[]): Promise<void> {
+
+        const contact = await msg.getContact();
+        if (!chat.isGroup || BANNED_USERS.includes(contact.number)) {
+            return;
+        }
 
         const key = `${chat.id}-${msg.author}`
         const now = Date.now();
