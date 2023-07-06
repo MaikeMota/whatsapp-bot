cbe40092302a43cd4b511cf4c86e422a97049da5
Maike Mota <maike_henrique@hotmail.com>
2023-07-06 14:01:41 -0300

    feat: allows to get all tickers from a prefix

--- src/commands/ticker.ts
+++ src/commands/ticker.ts
@@ -1,23 +1,37 @@
 import { Chat, Client, Message } from "whatsapp-web.js";
 import { Command } from "./command";
 
-import { getStockInfo } from "../services/brapi.service";
+import { getAvailableTickersFor, getStockInfo } from "../services/brapi.service";
 import { StockInfo } from "../services/stock-info.interface";
-import { hasCategorySuffix, tickerInfoToOneLineString } from "../utils/ticker.util";
+import { hasCategorySuffix, sortByMostNegativeDailyChange, tickerInfoToOneLineString } from "../utils/ticker.util";
 
+
+const availableTickerFor = new Map<string, string[]>();
 
 export class TickerCommand extends Command {
     command = '/ticker';
     alternativeCommands = ['/cotação'];
-    
+
     usageDescription = '<ticker> - Obtem a cotação do ativo. Exemplo: /ticker bbas3';
 
     async isValid(chat: Chat, msg: Message, ...argsArray: string[]): Promise<boolean> {
         const [ticker] = argsArray;
-        return !!ticker && hasCategorySuffix(ticker);
+        return !!ticker;
     }
-    
+
     async handle(client: Client, chat: Chat, msg: Message, ...argsArray: string[]): Promise<void> {
+
+        const ticker = argsArray[0];
+        
+        if (!hasCategorySuffix(ticker)) {
+            let tickersToAdd = availableTickerFor.get(ticker)
+            if (!tickersToAdd || tickersToAdd.length === 0) {
+                tickersToAdd = await getAvailableTickersFor(ticker);
+                availableTickerFor.set(ticker, tickersToAdd);
+            }
+            argsArray = [...argsArray.filter(a => a !== ticker), ...tickersToAdd]
+        }
+
         const isMultiple = argsArray.length > 1
 
         if (isMultiple) {
@@ -29,8 +43,6 @@ export class TickerCommand extends Command {
         }
 
         const [args] = argsArray;
-
-        const ticker = args.toLowerCase();
         const ticksInfo = await getStockInfo([ticker]);
         const tickInfo = ticksInfo.success[0];
         if (!tickInfo) {
@@ -42,10 +54,23 @@ export class TickerCommand extends Command {
     }
 
     private async handleMultipleTickers(argsArray: string[]): Promise<string> {
-        const results = await getStockInfo(argsArray);
+        let tickersToRequest = [];
+        for (const ticker of argsArray) {
+            if (!hasCategorySuffix(ticker)) {
+                let tickersToAdd = availableTickerFor.get(ticker)
+                if (!tickersToAdd || tickersToAdd.length === 0) {
+                    tickersToAdd = await getAvailableTickersFor(ticker);
+                    availableTickerFor.set(ticker, tickersToAdd);
+                }
+                tickersToRequest = [...tickersToRequest.filter(a => a !== ticker), ...tickersToAdd]
+            }else { 
+                tickersToRequest.push(ticker);
+            }
+        }
+        const results = await getStockInfo(tickersToRequest);
 
         let tickersMessage = "";
-        for (const result of results.success) {
+        for (const result of results.success.sort(sortByMostNegativeDailyChange)) {
             tickersMessage += this.getOneLineTickerMessage(result) + "\n";
         }
         return tickersMessage;
