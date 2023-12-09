b59ffe0bb25e6feb14f3756e53d95dd61d8b6952
Maike Mota <maike_henrique@hotmail.com>
2023-12-09 20:48:05 -0300

    feat: improve message

--- src/commands/wallet/wallet.buy.command.ts
+++ src/commands/wallet/wallet.buy.command.ts
@@ -44,7 +44,7 @@ export class WalletBuyCommand extends Command {
 
             await this.walletService.updatePosition(contactId, previousPosition);
 
-            await msg.reply('Compra registrada com sucesso! Veja sua nova posição: ')
+            await msg.reply('Compra registrada com sucesso! Sua nova posição: ')
             return  new WalletVerCommand().handle(client, chat, msg, previousPosition.ticker)
         } catch (error) {
             await msg.reply(error);
