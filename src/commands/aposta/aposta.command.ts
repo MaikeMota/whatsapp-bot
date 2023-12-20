2ea8a43e3113ee856c3b42023121dc27f18e4582
Maike Mota <maike_henrique@hotmail.com>
2023-12-20 14:28:57 -0300

    feat: allow change total number of games and total of numbers to generate

--- src/commands/aposta/aposta.command.ts
+++ src/commands/aposta/aposta.command.ts
@@ -1,6 +1,6 @@
 import { Client, GroupChat, Message } from "whatsapp-web.js";
 import { randomIntFromInterval } from "../../utils/util";
-import { bold } from "../../utils/whatsapp.util";
+import { bold, italic } from "../../utils/whatsapp.util";
 import { Command } from "../command";
 
 export class ApostaCommand extends Command {
@@ -8,18 +8,49 @@ export class ApostaCommand extends Command {
     alternativeCommands = []
 
     async handle(client: Client, chat: GroupChat, msg: Message, ...argsArray: string[]): Promise<void> {
-        const numbers: number[] = [];
-        for (let i = 0; i < 6; i++) {
 
-            let number = randomIntFromInterval(1, 60);
-            while (numbers.includes(number)) {
-                number = randomIntFromInterval(1, 60);
+
+        const [totalNumbersStr, totalGamesStr] = argsArray.filter(t => !!t)
+
+        let totalNumbers = 6
+        let totalGames = 1
+
+        if (totalNumbersStr) {
+            const tn = parseInt(totalNumbersStr)
+            if (!isNaN(tn)) {
+                totalNumbers = tn
             }
-            numbers.push(number);
         }
 
-        numbers.sort((a, b) => a - b)
-        await msg.reply(bold(numbers.join(", ")));
+        if (totalGamesStr) {
+            const tg = parseInt(totalGamesStr)
+            if (!isNaN(tg)) {
+                totalGames = tg
+            }
+        }
+
+        const games: number[][] = [][];
+        for (let i = 0; i < totalGames; i++) {
+            const numbers: number[] = [];
+            for (let i = 0; i < totalNumbers; i++) {
+                let number = randomIntFromInterval(1, 60);
+                while (numbers.includes(number)) {
+                    number = randomIntFromInterval(1, 60);
+                }
+                numbers.push(number);
+            }
+            numbers.sort((a, b) => a - b)
+            games.push(numbers)
+        }
+
+        const replyMessages = []
+        for (const g = 0; g < games.length; g++) {
+            replyMessages.push(italic(bold(`[Jogo #${g + 1}]`)))
+            replyMessages.push(bold(games[g].join(", ")))
+            replyMessages.push("")
+        }
+
+        await msg.reply(replyMessages.join("\n"));
     }
 
     get isV2() {
