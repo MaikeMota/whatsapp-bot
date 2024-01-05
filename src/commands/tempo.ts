00546f2c8d3c8649719f857aa47c7fa91d7cb5ce
Maike Mota <maike_henrique@hotmail.com>
2024-01-05 14:01:12 -0300

    fix: prevent query param override from user

--- src/commands/tempo.ts
+++ src/commands/tempo.ts
@@ -5,19 +5,32 @@ const { getWheater } = require('../services/wheater.service');
 
 export class TempoCommand extends Command {
     command = '/tempo';
-    
+
     usageDescription = '<cidade> - Recupera a previsão do tempo para a cidade informada.'
-    
+
     async isValid(chat: Chat, msg: Message, ...argsArray: string[]): Promise<boolean> {
         return argsArray.length > 0
     }
-    
+
     async handle(client: Client, chat: Chat, msg: Message, ...argsArray: string[]): Promise<void> {
-        let cidade = argsArray.length > 1 ? argsArray.join(' ') : argsArray[0];
+        let cidade = argsArray.join(' ');
+
+        const ampersandIndex = cidade.indexOf('&');
+        const hasAmpersand = ampersandIndex > -1;
+        if (hasAmpersand) {
+            await msg.reply("tô vendo essa zoeira ai!!!!", (await (msg.getChat())).id._serialized);
+            cidade = cidade.substring(0, ampersandIndex).trim();
+        }
+
+        if(!hasAmpersand && cidade.length === 0) {
+            await msg.reply("Você precisa informar uma cidade para eu consultar a previsão do tempo.");
+            return;
+        }
         const wheaterInfo = await getWheater(cidade);
 
-        if (!wheaterInfo) {
+        if (!hasAmpersand && !wheaterInfo) {
             await msg.reply(`Não consegui encontrar detalhes do tempo para a cidade de *${cidade}*, tente digitar o nome completo, sem abreviações.`);
+            return
         }
 
         const { temp, temp_min, temp_max, feels_like, humidity } = wheaterInfo;
