174e2b4cfced51bfaedcce935d4ec812e149cdd5
Maike Mota <maike_henrique@hotmail.com>
2024-01-30 21:39:30 -0300

    feat: allows to user send a id to be requested instead of registered one.

--- src/commands/strava/strava.list.command.ts
+++ src/commands/strava/strava.list.command.ts
@@ -7,29 +7,38 @@ import { Command } from "../command";
 import { StravaState } from "./strava.state";
 
 export class StravaListCommand extends Command {
-    
+
     command: string = "ver";
     description: string = "Lista o ranking do Strava para o grupo registrado para o chat";
 
     stateSaver: StateSaver<StravaState> = new JSONStateSaver<StravaState>();
 
 
-    async handle(client: Client, chat: Chat, msg: Message, ...[command, ...argsArray]: string[]): Promise<void> {
+    async handle(client: Client, chat: Chat, msg: Message, ...argsArray: string[]): Promise<void> {
+
+        const [id] = argsArray;
 
         const state = await this.stateSaver.load("strava");
-
-        if(!state || !state[chat.id._serialized]){
+        const clubId = id || state[chat.id._serialized]
+        if (!clubId) {
             await msg.reply("Nenhum grupo registrado para este chat")
             return
         }
 
-        let response = await StravaService.requestLeaderBoard(state[chat.id._serialized]);
-        const leadboard = response.data.sort((a, b) => a.rank - b.rank)
-        const messages = leadboard.map(item => `${bold(item.rank + "º")} - ${item.athlete_firstname} ${item.athlete_lastname} - ${StravaService.asHoursAndMinutes(item.moving_time)} - ${(item.distance / 1000).toFixed(1)}km`)
-        await msg.reply(messages.join('\n'));
+        try {
+            let response = await StravaService.requestLeaderBoard(clubId);
+            if (response.data.length === 0) {
+                await msg.reply(`Não há dados de atividades nesta semana para este grupo (${clubId}).`)
+                return
+            }
 
-        return
+            const leadboard = response.data.sort((a, b) => a.rank - b.rank)
+            const messages = leadboard.map(item => `${bold(item.rank + "º")} - ${item.athlete_firstname} ${item.athlete_lastname} - ${StravaService.asHoursAndMinutes(item.moving_time)} - ${(item.distance / 1000).toFixed(1)}km`)
+            await msg.reply(messages.join('\n'));
+        } catch (error) {
+            await msg.reply(`Erro ao buscar dados do Strava para o grupo ${id}.
+${error.message}`)
+        }
 
     }
-
 }
