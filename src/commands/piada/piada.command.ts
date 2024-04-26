e0edf32376542793e74bd3976cb17de9982ec212
Maike Mota <maike_henrique@hotmail.com>
2024-04-26 16:20:51 -0300

    enhance: envia uma foto aleatória de um gato de óculos

--- src/commands/piada/piada.command.ts
+++ src/commands/piada/piada.command.ts
@@ -1,4 +1,4 @@
-import { Client, GroupChat, Message } from "whatsapp-web.js";
+import { Client, GroupChat, Message, MessageMedia } from "whatsapp-web.js";
 import { Command } from "../command";
 
 
@@ -17,14 +17,17 @@ export class PiadaCommand extends Command {
 
     async handle(client: Client, chat: GroupChat, msg: Message, ...argsArray: string[]): Promise<void> {
 
+
+        const catApiResponse = await fetch('https://api.thecatapi.com/v1/images/search?size=med&mime_types=jpg&order=RANDOM&limit=1&category_ids=4&format=json');
+        const media = await MessageMedia.fromUrl(catApiResponse[0].url);
         const piada = getRandomElement(this.piadas);
         msg.reply(`${bold(piada.pergunta)}
 
 ${piada.resposta}
 
-${getRandomElement(["😄", "😊", "😃", "😁", "😆", "😀", "🤡", "🤣", "😅", "🙃", "😶‍🌫️", "🐦", "🤪", "😜", "😝", "😛", "😌", "🤓", "🥳","🥸"])}
-`
+${getRandomElement(["😄", "😊", "😃", "😁", "😆", "😀", "🤡", "🤣", "😅", "🙃", "😶‍🌫️", "🐦", "🤪", "😜", "😝", "😛", "😌", "🤓", "🥳", "🥸"])}`, chat.id._serialized, {
+            media: media
+        });
 
-        );
     }
 }
