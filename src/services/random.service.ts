e98866c52e0e5b8a591fababaf64360ac5e00f32
Maike Mota <maike_henrique@hotmail.com>
2024-05-02 14:12:15 -0300

    enhance: improve error handling

--- src/services/random.service.ts
+++ src/services/random.service.ts
@@ -19,12 +19,22 @@ export class RandomService {
             "id": 1
         }
 
-        return await fetch('https://api.random.org/json-rpc/2/invoke', {
-            body: JSON.stringify(payload),
-            headers: headers,
-            method: 'POST'
-        })
-            .then(r => r.json())
-            .then(r => r.result.random.data as number[]);
+        try { 
+            const response = await fetch('https://api.random.org/json-rpc/2/invoke', {
+                body: JSON.stringify(payload),
+                headers: headers,
+                method: 'POST'
+            })
+
+            if(response.status !== 200) {
+                console.log(`[RandomService] Failed to fetch random numbers. Status: ${response.status}`);
+                throw new Error('Failed to fetch random numbers');
+            }
+            return (await response.json()).result.random.data
+
+        }catch(e) { 
+            console.log(`[RandomService] Failed to fetch random numbers. Error: ${e}`);
+            throw new Error('Failed to fetch random numbers. Error: ' + e.message);
+        }
     }
 }
