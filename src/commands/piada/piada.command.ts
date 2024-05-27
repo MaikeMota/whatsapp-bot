9afb1b04f1b9e6daba26fe47c2b6ee84b5c9698c
Maike Mota <maike_henrique@hotmail.com>
2024-05-27 12:46:23 -0300

    upgrade to alpha6

--- src/commands/piada/piada.command.ts
+++ src/commands/piada/piada.command.ts
@@ -9,17 +9,20 @@ import { Piada } from "./piada.interface";
 
 export class PiadaCommand extends Command {
     command = '/piada';
-
     alternativeCommands = []
 
 
     piadas: Piada[] = piadas as Piada[]
 
     async handle(client: Client, chat: GroupChat, msg: Message, ...argsArray: string[]): Promise<void> {
+        let media: MessageMedia;
+        try { 
+            const catApiResponse = await fetch('https://api.thecatapi.com/v1/images/search?size=med&mime_types=jpg&order=RANDOM&limit=1&category_ids=4&format=json').then(r => r.json());
+            const media = await MessageMedia.fromUrl(catApiResponse[0].url);
+        }catch(exception) {
+            console.error("Erro ao buscar imagem de gato.\n" + exception)
+        }
 
-
-        const catApiResponse = await fetch('https://api.thecatapi.com/v1/images/search?size=med&mime_types=jpg&order=RANDOM&limit=1&category_ids=4&format=json').then(r => r.json());
-        const media = await MessageMedia.fromUrl(catApiResponse[0].url);
         const piada = getRandomElement(this.piadas);
         msg.reply(`${bold(piada.pergunta)}
 
