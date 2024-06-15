b26474456f7142d4381f9173a36dcb061d8a57be
Maike Mota <maike_henrique@hotmail.com>
2024-06-15 15:21:47 -0300

    fix: return an empty object when loading

--- src/utils/json-state-saver.ts
+++ src/utils/json-state-saver.ts
@@ -26,6 +26,7 @@ export class JSONStateSaver<T> implements StateSaver<T> {
             const json = readFileSync(filePath, { encoding: FILE_ENCODING });
             return JSON.parse(json) as T;
         }
+        return {} as T;
     }
 
     private resolveFilePath(key: string) {
