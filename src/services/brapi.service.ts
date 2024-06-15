3110fd7a231e8791f0dbc6be8f30ebe1f38215cc
Maike Mota <maike_henrique@hotmail.com>
2024-06-15 17:27:10 -0300

    feat: added BRAPI auth token

--- src/services/brapi.service.ts
+++ src/services/brapi.service.ts
@@ -1,11 +1,13 @@
 import { FinancialAPIRequestResult } from "./financial-api-request-result.interface";
 import { StockInfo } from "./stock-info.interface";
 
-
+const BRAPI_KEY = process.env.BRAPI_KEY;
 
 export async function getStockInfo(tickers: string[]): Promise<FinancialAPIRequestResult<StockInfo>> {
 
-    const apiResult = await fetch(`https://brapi.dev/api/quote/${tickers.join(',')}?interval=1d`).then(r => r.json() as Promise<BRAPIResponse>);
+    const apiResult = await fetch(`https://brapi.dev/api/quote/${tickers.join(',')}?interval=1d`, {
+        headers: getHeaders()
+    }).then(r => r.json() as Promise<BRAPIResponse>);
 
     const results: FinancialAPIRequestResult<StockInfo> = {
         success: [],
@@ -34,9 +36,16 @@ export async function getStockInfo(tickers: string[]): Promise<FinancialAPIReque
 
 }
 
+function getHeaders() {
+    const headers = new Headers();
+    headers.append('Authorization', `Bearer ${BRAPI_KEY}`)
+    return headers;
+}
+
 export async function getAvailableTickersFor(prefix: string): Promise<string[]> {
-    const apiResult = await fetch(`https://brapi.dev/api/available?search=${prefix}`)
-        .then(r => r.json() as Promise<BRAPITickerAvailablesResponse>);
+    const apiResult = await fetch(`https://brapi.dev/api/available?search=${prefix}`, {
+        headers: getHeaders()
+    }).then(r => r.json() as Promise<BRAPITickerAvailablesResponse>);
     return apiResult.stocks.filter(s => !s.toLowerCase().endsWith("f"));
 }
 
