000b7617ec7ecf2cc0756836db3c5cee3c77584c
Maike Mota <maike_henrique@hotmail.com>
2024-09-26 14:12:05 -0300

    feat: reenables /radar vda

--- src/commands/radar/radar.vda.command.ts
+++ src/commands/radar/radar.vda.command.ts
@@ -1,6 +1,10 @@
 import { Chat, Client, Message } from "whatsapp-web.js";
+import { getStockInfo } from "../../services/fcs-api.service";
+import { StockInfo } from "../../services/stock-info.interface";
 import { StateSaver } from "../../utils/interfaces/state-save.interface";
 import { JSONStateSaver } from "../../utils/json-state-saver";
+import { sortByMostNegativeDailyChange, tickerInfoToOneLineString } from "../../utils/ticker.util";
+import { bold } from "../../utils/whatsapp.util";
 import { Command } from "../command";
 
 
@@ -19,7 +23,30 @@ export class RadarVDACommand extends Command {
 
 
     async handle(client: Client, chat: Chat, msg: Message, ...argsArray: string[]): Promise<void> {
-        msg.reply("Comando desabilitado.")
+        const currState = await this.getState();
+
+        const tickers = currState.flat();
+
+        const tickersInfo = await getStockInfo(tickers);
+        const message = ["Cotações das empresas do seu Radar:\n"]
+        const stockInfos: StockInfo[] = []
+        for (const ticker of tickers) {
+            const info = tickersInfo.success.find(ti => ti.ticker === ticker.toUpperCase());
+            if (!info) {
+                continue;
+            }
+            stockInfos.push(info)
+        }
+        stockInfos
+            .sort(sortByMostNegativeDailyChange)
+            .forEach(info => message.push("\t" + tickerInfoToOneLineString(info)));
+
+        if (tickersInfo.failed.length > 0) {
+            message.push("\nNão foi possível recuperar as cotações dos seguintes tickers: " + tickersInfo.failed.map(t => bold(t)).join(", "))
+        }
+
+        message.push("\n** As cotações demonstradas possuem até 1 hora de atraso.")
+        msg.reply(message.join("\n"));
     }
 
     private async getState(): Promise<Array<string>> {
