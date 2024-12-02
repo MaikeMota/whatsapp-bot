db6a223b9a1232775cfe19b6af4a30e6f6234220
Maike Mota <maike_henrique@hotmail.com>
2024-12-02 18:39:16 -0300

    feat: uses new array of string to mention others

--- src/commands/all.ts
+++ src/commands/all.ts
@@ -43,8 +43,7 @@ export class MentionAllCommand extends Command {
         let mentions = [];
 
         for (let participant of chat.participants) {
-            const contact = await client.getContactById(participant.id._serialized);
-            mentions.push(contact);
+            mentions.push(participant.id._serialized);
             text += ` @${participant.id.user}`;
         }
         this.setLastUsage(key, now);
