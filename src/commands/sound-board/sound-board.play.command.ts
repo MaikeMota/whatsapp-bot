f1b94d5e72dcdf3e7b10dd37d577f477a0e31ea9
Maike Mota <maike_henrique@hotmail.com>
2025-02-28 10:01:17 -0300

    fix: reproduction increment

--- src/commands/sound-board/sound-board.play.command.ts
+++ src/commands/sound-board/sound-board.play.command.ts
@@ -44,8 +44,8 @@ export class SoundBoardPlayCommand extends Command {
         const sound = new MessageMedia(soundBoardState[soundKey].mimetype || 'audio/mpeg', soundBoardState[soundKey].data);
         console.log(`Playing sound ${soundKey}`);
         await (referencedMessage || msg).reply("", chat.id._serialized, { media: sound });
-        soundBoardState[soundKey].reproductionCount++;
-        this.stateSaver.save('soundboard', soundBoardState);
+        soundBoardState[soundKey].reproductionCount? soundBoardState[soundKey].reproductionCount = 1 : soundBoardState[soundKey].reproductionCount++;
+        await this.stateSaver.save('soundboard', soundBoardState);
         console.log(`Sound ${soundKey} sent`);
     }
 
