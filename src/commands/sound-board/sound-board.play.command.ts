7b63f2e4e6360b84653bea355dbfd3ab79ceb040
Maike Mota <maike_henrique@hotmail.com>
2025-03-15 16:58:59 -0300

    fix: fix reproduction count

--- src/commands/sound-board/sound-board.play.command.ts
+++ src/commands/sound-board/sound-board.play.command.ts
@@ -45,7 +45,7 @@ export class SoundBoardPlayCommand extends Command {
         const sound = new MessageMedia(soundBoardState[soundKey].mimetype || 'audio/mpeg', soundBoardState[soundKey].data);
         console.log(`Playing sound ${soundKey}`);
         await (referencedMessage || msg).reply("", chat.id._serialized, { media: sound });
-        soundBoardState[soundKey].reproductionCount? soundBoardState[soundKey].reproductionCount = 1 : soundBoardState[soundKey].reproductionCount++;
+        soundBoardState[soundKey].reproductionCount? soundBoardState[soundKey].reproductionCount++ : soundBoardState[soundKey].reproductionCount = 1;
         await this.stateSaver.save('soundboard', soundBoardState);
         console.log(`Sound ${soundKey} sent`);
     }
