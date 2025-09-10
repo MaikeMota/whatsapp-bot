6c030dc0a691c5347e6745ff1e598474bbd3f76d
Maike Mota <maike_henrique@hotmail.com>
2025-09-10 10:08:33 -0300

    feat: improve youtube video resume feature

--- src/commands/gemini/youtube-video-resume.command.ts
+++ src/commands/gemini/youtube-video-resume.command.ts
@@ -2,7 +2,7 @@ import { Client, GroupChat, Message } from "whatsapp-web.js";
 import { Command } from "../command";
 
 
-const { GEMINI_KEY, GEMINI_RESUMO_PROMPT, GEMINI_YOUTUBE_URL_REGEX } = process.env;
+const { GEMINI_KEY, GEMINI_DEFAULT_MODEL, GEMINI_YOUTUBE_URL_REGEX, GEMINI_VIDEO_RESUMO_SYSTEM_INSTRUCTION } = process.env;
 
 const YOUTUBE_URL_REGEX = new RegExp(GEMINI_YOUTUBE_URL_REGEX);
 
@@ -12,55 +12,78 @@ export class YoutubeVideoResumeCommand extends Command {
 
     async handle(client: Client, chat: GroupChat, msg: Message, ...argsArray: string[]): Promise<void> {
 
-        let [url] = argsArray;
+        let videoUrl;
+        let model;
+        let extraInstructions;
 
         const quotedMessage = await msg.getQuotedMessage();
         const hasQuotedMsg = !!quotedMessage;
         if (hasQuotedMsg) {
-            const quotedMsg = await msg.getQuotedMessage();
-            const matchs = quotedMsg.body.match(YOUTUBE_URL_REGEX)
-            url = matchs[0];
+            videoUrl = quotedMessage.body.trim();
+            model = argsArray[0] || GEMINI_DEFAULT_MODEL;
+            extraInstructions = argsArray.slice(1).join(" ");
+        }else {
+            videoUrl = argsArray[0];
+            model = argsArray[1] || GEMINI_DEFAULT_MODEL;
+            extraInstructions = argsArray.slice(2).join(" ");
         }
 
-        if (!url) {
+        if (!videoUrl) {
             await msg.reply("Informe a url do video");
             return;
         }
-        if (!url.match(YOUTUBE_URL_REGEX)) {
+        if (!videoUrl.match(YOUTUBE_URL_REGEX)) {
             await msg.reply("Url inválida, por favor, usar somente link do youtube para vídeos públicos.");
             return;
         }
 
+        const videoId = videoUrl.match(YOUTUBE_URL_REGEX)[1];
+
+
         const myHeaders = new Headers();
         myHeaders.append("Content-Type", "application/json");
-        const body = JSON.stringify({
-            "contents": [
+        const system_instruction = { 
+            "parts": [
                 {
-                    "parts": [
-                        {
-                            "text": GEMINI_RESUMO_PROMPT
-                        },
-                        {
-                            "file_data": {
-                                "file_uri": url
-                            }
-                        }
-                    ]
+                    "text": GEMINI_VIDEO_RESUMO_SYSTEM_INSTRUCTION
                 }
             ]
+        };
+
+        const contents = [];
+        const parts = [];
+        contents.push({parts})
+        if (extraInstructions) {
+            parts.push({ text: extraInstructions });
+        }
+        parts.push({
+            "file_data": {
+                "file_uri": `https://youtu.be/${videoId}`
+            }
+        });
+
+        const body = JSON.stringify({
+            system_instruction,
+            contents,
         });
 
         const resultMessage = await msg.reply("Processando...");
 
-        const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${GEMINI_KEY}`, {
+       await fetch(`https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${GEMINI_KEY}`, {
             method: "POST",
             headers: myHeaders,
             body
         }).then((response) => response.json())
-        .catch((error) => console.error(error));
-
+        .then(async (response) => {
+            if(response.error) { 
+                throw new Error(response.error.message);
+            }
             await resultMessage.edit(response.candidates[0].content.parts[0].text);
-
+        })
+        .catch((error) => { 
+            console.error(error);
+            resultMessage.edit("Erro ao processar o vídeo, tente novamente mais tarde.\nError: " + error.message);
+        });
 
     }
 }
