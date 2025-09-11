93119335526be206bbeec64ce6d1079df93a88bf
Maike Mota <maike_henrique@hotmail.com>
2025-09-11 13:16:41 -0300

    feat: added support for small video messages

--- src/commands/gemini/youtube-video-resume.command.ts
+++ src/commands/gemini/youtube-video-resume.command.ts
@@ -1,4 +1,4 @@
-import { Client, GroupChat, Message } from "whatsapp-web.js";
+import { Client, GroupChat, Message, MessageTypes } from "whatsapp-web.js";
 import { Command } from "../command";
 
 
@@ -15,30 +15,37 @@ export class YoutubeVideoResumeCommand extends Command {
         let videoUrl;
         let model;
         let extraInstructions;
+        let inlineData;
 
         const quotedMessage = await msg.getQuotedMessage();
         const hasQuotedMsg = !!quotedMessage;
         if (hasQuotedMsg) {
-            videoUrl = quotedMessage.body.trim();
+
             model = argsArray[0] || GEMINI_DEFAULT_MODEL;
             extraInstructions = argsArray.slice(1).join(" ");
-        }else {
+
+            if(quotedMessage.type === MessageTypes.TEXT){
+                videoUrl = quotedMessage.body.trim();
+            } else if (quotedMessage.type === MessageTypes.VIDEO) {
+                const media = await quotedMessage.downloadMedia();
+                if (media) {
+                    if (!media.mimetype.startsWith("video/")) {
+                        await msg.reply("Por favor, envie um vídeo ou link do youtube.");
+                    }
+                    inlineData = { 
+                        mimeType: media.mimetype, 
+                        data: media.data,
+                        size: media.filesize
+                    };
+                }
+                await msg.reply("Não foi possível realizar o download do vídeo.")
+                return;
+            }
+         } else {
             videoUrl = argsArray[0];
             model = argsArray[1] || GEMINI_DEFAULT_MODEL;
             extraInstructions = argsArray.slice(2).join(" ");
         }
-
-        if (!videoUrl) {
-            await msg.reply("Informe a url do video");
-            return;
-        }
-        if (!videoUrl.match(YOUTUBE_URL_REGEX)) {
-            await msg.reply("Url inválida, por favor, usar somente link do youtube para vídeos públicos.");
-            return;
-        }
-
-        const videoId = videoUrl.match(YOUTUBE_URL_REGEX)[1];
-
 
         const myHeaders = new Headers();
         myHeaders.append("Content-Type", "application/json");
@@ -52,16 +59,40 @@ export class YoutubeVideoResumeCommand extends Command {
 
         const contents = [];
         const parts = [];
-        contents.push({parts})
+
+        if(inlineData) {
+            if(inlineData.size && inlineData.size >= 2e+7) { 
+                await msg.reply("Vídeo muito grande, por favor envie um vídeo menor que 20MB ou link do youtube.");
+            }
+            parts.push({
+                "inline_data": {
+                    "mime_type": inlineData.mimeType,
+                    "data": inlineData.data,
+                }
+            });
+        } else {
+            if (!videoUrl) {
+                await msg.reply("Informe a url do video");
+                return;
+            }
+            if (!videoUrl.match(YOUTUBE_URL_REGEX)) {
+                await msg.reply("Url inválida, por favor, usar somente link do youtube para vídeos públicos.");
+                return;
+            }
+
+            const videoId = videoUrl.match(YOUTUBE_URL_REGEX)[1];
+            parts.push({
+                "file_data": {
+                    "file_uri": `https://youtu.be/${videoId}`
+                }
+            });
+        }
+
+        contents.push({parts});
+        
         if (extraInstructions) {
             parts.push({ text: extraInstructions });
         }
-        parts.push({
-            "file_data": {
-                "file_uri": `https://youtu.be/${videoId}`
-            }
-        });
-
         const body = JSON.stringify({
             system_instruction,
             contents,
